6557c5d4416572ceb6a045d565416e0f2f612b79
AshanteNicole <Ashante.Spencer@hotmail.com>
2018-11-27 20:13:01 -0800

    Ashante version

--- public/js/editbook.js
+++ public/js/editbook.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     var rack_location = $("#fracklocation")
     var edition_number = $("#fedition")
     var sessionBookid = "";
+    var stock_status = $("#status");
 
     $(document).on("submit", "#book-frm-id", handleBookFetchSubmit);
     $(document).on("submit", "#book-frm", handleBookFormSubmit);
@@ -23,6 +24,7 @@ $(document).ready(function () {
             $("#fracklocation").val(bookdetail.rack_location);
             $("#fedition").val(bookdetail.edition_number);
             sessionBookid = bookdetail.id;
+            $("#status").val(stock_status);
         })
     };
 
@@ -36,6 +38,8 @@ $(document).ready(function () {
             book_summary: book_summary.val().trim(),
             rack_location: rack_location.val().trim(),
             edition_number: edition_number.val().trim(),
+            stock_status: stock_status.val().trim(),
+
             
         });
 
@@ -43,7 +47,7 @@ $(document).ready(function () {
 
     function updateBook(bookData) {
         console.log("Session book id " + sessionBookid);
-        alert("Book record updated successfully");
+        
         console.log(bookData);
 
 
